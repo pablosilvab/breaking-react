e298ab1035cca5ec4702e2d98bc452f2aecb3239
Pablo Silva <pablonicolassilvabravo@gmail.com>
2020-06-10 22:16:29 -0400

    Se agrega boton

---
+++ README.md
@@ -0,0 +1,6 @@
+# Breaking React
+
+El objetivo de esta App es mostrar frases de Breaking Bad consumiendo una [API](https://breaking-bad-quotes.herokuapp.com/v1/quotes). 
+
+## Requisitos
+
--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React from "react";
+import styled from "@emotion/styled";
+
+const Contenedor = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+`;
+
+const Boton = styled.button`
+  background: -webkit-linear-gradient(
+    top left,
+    #007d35 0%,
+    #007d35 40%,
+    #0f574e 100%
+  );
+  background-size: 300px;
+  font-family: Ariel, Helvetiva, sans-serif;
+  color: #fff;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+`;
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Contenedor>
+      <Boton>Obtener frase</Boton>
+    </Contenedor>
+  
   );
 }
 
